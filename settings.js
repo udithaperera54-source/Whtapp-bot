cb64545d15f0987b1abdb849efeada2a226ded15
udithaperera54-source <udithaperera54@gmail.com>
2026-01-03 10:03:40 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=177c8481bf3e29c9' : process.env.SESSION_ID,
+PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
+SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
+};
